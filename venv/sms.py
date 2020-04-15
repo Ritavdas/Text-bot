31a4f2ec4f256dece882f775fbe4f50f8191be7a
Ritav Das <45960527+Ritavdas@users.noreply.github.com>
2020-04-15 13:57:38 +0530

    Update sms.py

--- venv/sms.py
+++ venv/sms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = 'AC33b1631b7fb1bff8912dca2a15909cae'
-auth_token = '1dc3ab6e934b45c2112ae3ed8ce106f4'  # your auth token
+account_sid = ''
+auth_token = ''  # your auth token
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
-    body="HELLO KAUSIK DAS . YOU SEND SOMEONE DOWN TO GET FRUITS OR MEDICINES AGAIN . IT WILL BOT BE GOOD",
-    from_='+18507357744',
-    to="+919650098075"
+    body="HELLO. YOU SEND SOMEONE DOWN TO GET FRUITS OR MEDICINES AGAIN . IT WILL BOT BE GOOD",
+    from_='************', #enter twilio number here
+    to="***********" # enter number where you want to send text
 )
 
 print(message.sid)
